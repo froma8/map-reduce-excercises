72c48a98a5d705c18c6f32374c2ebb9e240cf36f
Federico Roma <federicoroma8@gmail.com>
2020-03-29 14:50:03 +0200

    Added wordcount excercise

---
+++ wordcount/mapper.py
@@ -0,0 +1,21 @@
+#!/home/federico/anaconda3/bin/python
+"""mapper.py"""
+
+import sys
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        print('%s\t%s' % (word, 1))
+
+# With generators:
+# [print(f'{word}\t1') for word in line.strip().split() for line in sys.stdin]
